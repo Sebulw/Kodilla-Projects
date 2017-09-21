9a3b80e07d6bffac1bc8a81f8cf400d1abdad184
Sebastian Wojnicz <sebulw@gmail.com>
2017-09-21 19:11:47 +0200

    Triangle calculator script added

--- JS modules/Zadanie 1/Workspace/JS/scripts.js
+++ JS modules/Zadanie 1/Workspace/JS/scripts.js
@@ -7,3 +7,8 @@ console.log('Hello, ' + name);
 	there is a story to tell
 */
 //	but not today :)
+var a = 4;
+var h = 3;
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
